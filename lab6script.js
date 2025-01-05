937af974b1fb9cde4523e6f91174206e0aafe398
Thuong23130327 <23130327@st.hcmuaf.edu.vn>
2025-01-06 02:19:31 +0700

    Your commit message

---
+++ lab6script.js
@@ -0,0 +1,9 @@
+// Example of a simple console log
+console.log("Page loaded");
+function showWebInterface() {
+    // Hide main content
+    document.getElementById('main-content').style.display = 'none';
+    // Show Web Development courses interface
+    document.getElementById('web-interface').style.display = 'block';
+}
+
